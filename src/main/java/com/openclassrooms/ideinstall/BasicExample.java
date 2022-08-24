1cc4d2f5820a3bfc0b58aecc7af71c6327b9409b
33667 <33667@192.168.1.95>
2022-08-24 17:52:07 +0200

    corrections

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -5,7 +5,8 @@ public class BasicExample{
 	public static void main(String[] args) {
 		int i;
 		for(i=0; i<5; i++) {
-			System.out.println("Hello bugs corrected by Acenboga !");
+			System.out.println("Hello bugs corrected by Acenboga  !");
 		}
 	}
 }
+#
